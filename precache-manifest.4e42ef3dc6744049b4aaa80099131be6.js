da177adc96e4be307a7d204f504a233ccda8d8f5
Shubham Wazir <SelfProclaimedDev@gmail.com>
2020-05-05 12:11:38 +0530

    Updates

---
+++ precache-manifest.4e42ef3dc6744049b4aaa80099131be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d6441da32e9c8c0634928c11894c7b4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fbb13deb5a78ef2fb854",
+    "url": "/monsters-rolodex/static/css/main.0e90e35d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb13deb5a78ef2fb854",
+    "url": "/monsters-rolodex/static/js/main.f81802f8.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
